22cd20b313c5c6ec56f6eaa7e77f2c4e1093a525
Daniel Molloy <danieltmolloy919@gmail.com>
2024-10-07 23:40:00 +1100

    try and do a 3x3 area

--- farm-setup.lua
+++ farm-setup.lua
@@ -15,3 +15,66 @@ turtle.placeDown()
 turtle.turnLeft()
 turtle.forward()
 turtle.turnRight()
+
+local function moveLeft()
+    turtle.turnLeft()
+    turtle.forward()
+    turtle.turnRight()
+end
+
+local function moveRight()
+    turtle.turnRight()
+    turtle.forward()
+    turtle.turnLeft()
+end
+
+local function placeFertilizer()
+    turtle.select(1)
+    turtle.placeDown()
+end
+
+
+local function checkFuel()
+    moveLeft()
+
+    if turtle.getFuelLevel() < 20000 then
+        print("Fuel level is low. Refueling...")
+        turtle.select(15)
+        turtle.suck(64)
+        turtle.refuel(64)
+    end
+
+    moveRight()
+    turtle.select(1)
+end
+
+local function stockUp()
+    turtle.select(1)
+    turtle.suck(64)
+end
+
+local function main()
+    checkFuel()
+    stockUp()
+
+    -- move up to place fertilizer
+    turtle.up()
+
+    -- turn around
+    turtle.turnLeft()
+    turtle.turnLeft()
+
+    -- place fertilizer in a 3x3 square
+    for i = 1, 3 do
+        for j = 1, 3 do
+            placeFertilizer()
+            moveRight()
+        end
+        turtle.back()
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnRight()
+    end
+end
+
+main()
