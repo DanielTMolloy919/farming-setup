317c77135fe24c4392d66e90b3c11d0f2ff8559c
Daniel Molloy <danieltmolloy919@gmail.com>
2024-10-07 23:07:10 +1100

    working fertilized farmland placement

--- farm-setup.lua
+++ farm-setup.lua
@@ -1,12 +1,14 @@
 -- Collect 1 item from the chest
+turtle.select(1)
 turtle.suck(1)
 
 -- Move left by one
-turtle.turnLeft()
-turtle.forward()
 turtle.turnRight()
+turtle.forward()
+turtle.turnLeft()
 
 -- Right click the block below with the item from the chest
+turtle.select(1)
 turtle.placeDown()
 
 -- Move back
